1d13db2d07077c5bbade12e22ebf57a760f040ab
Ante Perisic <perisic.ante@gmail.com>
2024-01-08 10:10:49 +0100

    add buff reader

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    let file = File::open(&args.path).expect("cant open file");
+    let mut b_reader = BufReader::new(file);
+
+    for line in b_reader.lines() {
+        if let Ok(xx) = line {
+            if xx.contains(&args.pattern) {
+                println!("{}", xx);
+            } else {
+                println!("Not found matching pattern");
+            }
+        } else {
+            println!("there is no lines here");
+        }
+    }
+}
