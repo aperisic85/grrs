da0a9334d8460b7cf022f2e20ec020537b6fec7a
Ante Perisic <perisic.ante@gmail.com>
2024-01-11 12:37:27 +0100

    add testing fn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use clap::Parser;
 use anyhow::{Context, Result};
+use clap::Parser;
+use std::io::{self,Write};
+use log::{info, warn};
 
 #[derive(Debug)]
 struct CustomError(String);
@@ -10,17 +12,42 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let args = Cli::parse();
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    writeln!(handle, "foo: {}", 42);
+
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file {}", args.path.display()))?;
-
-    for line in content.lines(){
-        if line.contains(&args.pattern){
-            println!("{}", line);
-        }
-    }
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
+}
+
+
+
+//std::out expect bytes so we need to use std::io::Write (u8) instead std::fmt::Write.
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write){
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer,"{}", line);
+        }
+    }
+}
+
+fn answer() -> u32{
+    43
+}
+
+#[test]
+fn find_a_match(){
+    let mut result  =Vec::new();
+    find_matches("where are\nyou now baby\nblah Mercedes benz", "where", &mut result);
+    assert_eq!(result, b"where are\n"); //fn find_matches() returns whole line so wee need to compare whole line not patter only
+}
+#[test]
+fn check_answer(){
+    assert_eq!(answer(), 43);
 }
